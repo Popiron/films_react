540ad783d8fe58b45ec2e42bcd55655282875149
Galushko DM <dim.galuschko@yandex.ru>
2022-03-03 21:25:27 +0300

    add movies page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import React from 'react';
 import {Navbar} from './components/navbar/navbar.js';
 import {MoviePoster} from './components/movie_poster/moviePoster.js';
 import Grid from '@mui/material/Grid';
-import { SearchBar } from './components/search_bar/search_bar';
-const welcome = {
-  greeting: "Hey",
-  title: 'React',
-}
+import { SearchBar } from './components/search_bar/search_bar.js';
 
 const API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YTAyZjM1ODk1ODlhZDhlNDQ4ODg4YTA2OWM0NjY0OSIsInN1YiI6IjVmZDVlMjk0NDE0MjkxMDAzZjQ1ZjYxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JPC-r71EYBFa-YmxB-VeZzfAowne0A_SQvYrBgCs07E";
 
@@ -110,15 +106,17 @@ const App = () => {
   return (
     <>
       <Navbar/>
+      <div className='main_title'>
+        Movies
+      </div>
       <SearchBar/>
+      <div className='search_count'>
+        120 items
+      </div>
       <div className='movies_grid'>
-      <Grid className="movies_grid" container direction="row" justifyContent="center" spacing={{ xs: 2, md:  65}} columns={{ xs: 4, sm: 4, md: 20 }}>
       {data.map((element, index) => (
-        <Grid  item container justifyContent="center" alignItems="flex-start" padding="0px 200px" sm={4} md={4} key={index}>
             <MoviePoster rating={element.rating} title={element.title} imgUrl={element.imgUrl}/>
-        </Grid>
       ))}
-      </Grid> 
       </div>
       
     </>
--- src/components/search_bar/search_bar.js
+++ src/components/search_bar/search_bar.js
@@ -1,8 +1,16 @@
+import SearchInput from 'react-search-input'
+import './search_bar.css';
+import {ReactComponent as SearchIcon} from  '../../images/search.svg';
+
+
 export const SearchBar = () => {
   return (
-      <input
-        type="text"
-        placeholder="Search Movies">
-      </input>
+    <div className='container'>
+        <input type="text" placeholder='Search Films' ></input>
+        <button>
+        <SearchIcon className='icon' color='#FFFFFF'/>
+        </button>
+
+    </div>
   );
 }
